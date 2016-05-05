1e56caabc7bd90dbea6dd9e52bac02f96d37c53a
morvinzha@outlook.com <morvinzha@outlook.com>
2016-05-05 17:00:44 +0800

    Add more options for plotting

--- DynamicSystemPlot.jl
+++ DynamicSystemPlot.jl
@@ -7,7 +7,7 @@ type DynamicSystem
 end
 
 #test test
-function RK4step( f::Function, x, h )
+function rk4step( f::Function, x, h )
 
    k1 = f(x);
    k2 = f(x+0.5*h*k1);
@@ -18,51 +18,53 @@ function RK4step( f::Function, x, h )
 end
 
 #bla blubb
-function RK4( f::Function, x0, T, N::Integer = 100)
+function rk4( f::Function, x0, t_end, npoint::Integer = 100)
 
    x = x0;
-   h = T/N;
-   for i=1:N
-        x += RK4step( f, x, h );
+   h = t_end/npoint;
+   for i=1:npoint
+        x += rk4step( f, x, h );
    end
    return x;
 end
 
 
 
-function DynamicSystemPlot(Dyn::DynamicSystem,Npoint=100,Npath=20,resol=0.01,width=0.05,Op="s")
-x0 = Dyn.x0
+function dynamicSystemPlot(dyn::DynamicSystem;npoint::Integer=100,npath::Integer=20,
+resol=0.01,width=0.05,start_time=0,rk4_npoint=100,option="s")
+
+x0 = dyn.x0
 function g(t)
-	return RK4(Dyn.f,x0,t)
+	return rk4(dyn.f,x0,t,rk4_npoint)
 end
 	st = "plot("
 	stend = ")"
-	lay = Array(AbstractString,Npath)
-	xaxis = Array(Float64,0)
-	yaxis = Array(Float64,0)
+	lay = Array(AbstractString,npath)
 
-	for i = 1:Npath
+	for i = 1:npath
 		xaxis = Array(Float64,1)
 		yaxis = Array(Float64,1)
-		x0 = [width*i+Dyn.x0[1];Dyn.x0[2]]
-		xaxis[1] = x0[1]
-		yaxis[1] = x0[2]
-		for j = 1:Npoint
-			Temp = g(resol*j)
+		x0 = [width*i+dyn.x0[1];dyn.x0[2]]
+		offset = g(start_time)
+		xaxis[1] = offset[1]
+		yaxis[1] = offset[2]
+		for j = 1:npoint
+			Temp = g(resol*j+start_time)
 			xaxis=[xaxis;Temp[1]]
 			yaxis=[yaxis;Temp[2]]
 		end
 		eval(Expr(:(=),parse("xaxis$i"),xaxis))
 		eval(Expr(:(=),parse("yaxis$i"),yaxis))
 		lay[i] = "layer(x=xaxis$i,y=yaxis$i,Geom.path)"
-		if i==Npath
+		if i==npath
 			st = string(st,lay[i],)
 		else
 			st = string(st,lay[i],",")
 		end
 	end
+
 	st = string(st,stend)
-	if Op == "PNG"
+	if option == "PNG"
 		p = eval(parse(st))
 		draw(PNG("DynamicSystem.png",12cm,12cm),p)
 	else
--- Readme.md
+++ Readme.md
@@ -8,12 +8,14 @@ Plotting : ``DynamicSystemPlot.jl`` is for visualization of a 2-D dynamic system
 ### Plotting
 All args of ```DynamicSystemPlot``` function are:
 ```
-Dyn::DynamicSystem
-Npoint=100                # How many points do you want to plot in each path?
-Npath=20                  # How many path do you want to plot in this graph?
+dyn::DynamicSystem
+npoint=100                # How many points do you want to plot in each path.
+npath=20                  # How many path do you want to plot in this graph.
 resol=0.01                # How "long"(time step) is each point to the next point.
-width=0.05                # How wide is the initial point of each path to the next one(along +x direction).  
-Op="s"                    # Another option is  Op = "PNG", to creat a .PNG file of this graph.
+width=0.05                # How wide is the initial point of each path to the next one(along +x direction).
+start_time=0,			  # How much time do you want to delay before plotting.
+rk4_npoint=100			  # How many times do you want RungeKutta-4 to compute for each point.
+option="s"                # Another option is  option = "PNG", to creat a .PNG file of this graph.
 ```
 #### Example
 **The Dynamic system is:**
@@ -42,6 +44,6 @@ DynamicSystemPlot(generic function with 1 method)
 julia> Dy = DynamicSystem(f,[-2;-2])
 DynamicSystem(f,[-2,-2])
 
-julia> DynamicSystemPlot(Dy,180,40,0.01,0.1)
+julia> DynamicSystemPlot(Dy , npoint = 180, npath = 40, resel = 0.01 , width = 0.1)
 ```
 ![In browser](DynPlotExample.png)
